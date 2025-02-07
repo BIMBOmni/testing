d4fe3457fc73ecdf0a7091270250bf00b1243017
robbieong <robbieong@192.168.4.84>
2025-02-07 17:49:12 +0800

    New addition including Android and Emulator testing sample scripts

---
+++ Test Suites/Android Test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Android Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2056a3bf-9ed3-4370-ba89-f419df8bf54a</testSuiteGuid>
+   <testCaseLink>
+      <guid>1c2ed7ec-e8d9-4ddd-9c42-60534459c4b1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Android Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
